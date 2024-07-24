d8ec1243593638bd0e1253908b41a654bc5625ac
sbussiso <sbussiso321@gmail.com>
2024-07-24 20:32:21 +0000

    added admin functions

--- app.py
+++ app.py
@@ -33,6 +33,11 @@ class UserHistory(db.Model):
     action = db.Column(db.String(150))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+class PlatformUpdates(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title =  db.Column(db.String(150), unique=True)
+    content = db.Column(db.String(150), unique=True)
+
 class UserRegistration(Resource):
     def post(self):
         try:
@@ -127,9 +132,91 @@ class RecordUserHistory(Resource):
         except Exception as e:
             return {"message": "Something went wrong"}, 500
 
+class ListUsers(Resource):
+    @jwt_required()
+    def get(self):
+        users = User.query.all()
+        return jsonify([{"id": user.id, "email": user.email, "username": user.username} for user in users])
+
+class SearchUsers(Resource):
+    @jwt_required()
+    def get(self):
+        username = request.args.get('username')
+        email = request.args.get('email')
+        user_id = request.args.get('id')
+        
+        if username:
+            user = User.query.filter_by(username=username).first()
+        elif email:
+            user = User.query.filter_by(email=email).first()
+        elif user_id:
+            user = User.query.filter_by(id=user_id).first()
+        else:
+            return {"message": "No search criteria provided"}, 400
+        
+        if user:
+            return {"id": user.id, "email": user.email, "username": user.username}
+        else:
+            return {"message": "User not found"}, 404
+
+class DeleteUser(Resource):
+    @jwt_required()
+    def delete(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return {"message": "User deleted"}, 200
+        else:
+            return {"message": "User not found"}, 404
+
+class ResetUserEmail(Resource):
+    @jwt_required()
+    def put(self, user_id):
+        new_email = request.json.get('new_email')
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            user.email = new_email
+            db.session.commit()
+            return {"message": "Email updated"}, 200
+        else:
+            return {"message": "User not found"}, 404
+
+class ResetUserPassword(Resource):
+    @jwt_required()
+    def put(self, user_id):
+        new_password = request.json.get('new_password')
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            user.password = generate_password_hash(new_password)
+            db.session.commit()
+            return {"message": "Password updated"}, 200
+        else:
+            return {"message": "User not found"}, 404
+
+class PlatformUpdatesResource(Resource):
+    @jwt_required()
+    def post(self):
+        data = request.get_json()
+        title = data.get('title')
+        content = data.get('content')
+        if not title or not content:
+            return {"message": "Title and content are required"}, 400
+        
+        update = PlatformUpdates(title=title, content=content)
+        db.session.add(update)
+        db.session.commit()
+        return {"message": "Update added"}, 201
+
 api.add_resource(UserRegistration, '/register')
 api.add_resource(UserLogin, '/login')
 api.add_resource(RecordUserHistory, '/user_history')
+api.add_resource(ListUsers, '/users')
+api.add_resource(SearchUsers, '/users/search')
+api.add_resource(DeleteUser, '/users/<int:user_id>')
+api.add_resource(ResetUserEmail, '/users/<int:user_id>/email')
+api.add_resource(ResetUserPassword, '/users/<int:user_id>/password')
+api.add_resource(PlatformUpdatesResource, '/platform_updates')
 
 if __name__ == '__main__':
     with app.app_context():
